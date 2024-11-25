38c8bf5db43adbc87e4542b851b79551b34f0f0c
Daniel Cruz López <danielcruzlopez1501@gmail.com>
2024-11-25 20:50:27 +0000

    Pet data show on screen

--- src/app/pages/pet-profile/pet-profile.component.ts
+++ src/app/pages/pet-profile/pet-profile.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core'
+import {Component, inject} from '@angular/core'
 import { Title, Meta } from '@angular/platform-browser'
+import {Observable} from "rxjs";
+import {UserService} from "../../services/user.service";
+import {AuthStateService} from "../../auth/data-access/auth-state.service";
+import {FirestoreService} from "../../services/firestore.service";
 
 @Component({
   selector: 'pet-profile',
@@ -8,6 +12,24 @@ import { Title, Meta } from '@angular/platform-browser'
 })
 export class PetProfile {
   raw1b3g: string = ' '
+  user$: Observable<any> | undefined;
+  petName: string = ' ';
+  petType: string = ' ';
+  petBreed: string = ' ';
+  petSex: string = ' ';
+  petStatus: string = ' ';
+  petWeight: string = ' ';
+  petSize: string = ' ';
+  petAge: string = ' ';
+  petDiseases: string = ' ';
+  petDescription: string = ' ';
+  petImage: string = ' ';
+  petSearch: string = ' ';
+
+  userId: string | null = null;
+  private _userService = inject(UserService);
+  private _authState = inject(AuthStateService);
+  private _fireService = inject(FirestoreService);
   constructor(private title: Title, private meta: Meta) {
     this.title.setTitle('Pet-Profile - PawMatch')
     this.meta.addTags([
@@ -17,4 +39,42 @@ export class PetProfile {
       },
     ])
   }
+
+  async ngOnInit(): Promise<void> {
+    await this.getUserId();
+    this.loadUser();
+  }
+
+  async getUserId(): Promise<void> {
+    const currentUser = await this._authState.currentUser;
+    if (currentUser && currentUser.uid) {
+      this.userId = currentUser.uid;
+      console.log('ID del usuario autenticado:', this.userId);
+    } else {
+      console.error('No hay usuario autenticado');
+    }
+  }
+
+  loadUser(): void {
+    if (this.userId) {
+      this.user$ = this._fireService.getDataFromCurrentAuthUser(this.userId);
+      this.user$.subscribe(user => {
+        if (user.profilePet && user.profilePet.length > 0) {
+          const pet = user.profilePet[0];
+          this.petName = pet.name || 'No name available';
+          this.petType = pet.species || 'Unknown type';
+          this.petBreed = pet.breed || 'Unknown breed';
+          this.petSex = pet.sex || 'Unknown sex';
+          this.petStatus = pet.sexualStatus || 'Unknown status';
+          this.petWeight = pet.weight || 'Unknown weight';
+          this.petSize = pet.size || 'Unknown size';
+          this.petAge = pet.age || 'Unknown age';
+          this.petDiseases = pet.diseases || 'No known diseases';
+          this.petDescription = pet.description || 'No description available';
+          this.petSearch = pet.search || 'No search info';
+        }
+      });
+    }
+  }
+
 }
